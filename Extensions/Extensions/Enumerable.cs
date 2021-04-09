eab3498f360ec5f1e6aa1afc9e1ecf0a3a3e403a
M. Gambill <46201338+mgambill@users.noreply.github.com>
2021-04-09 00:02:05 -0400

    Update Enumerable.cs
    Add EachAsync

--- Extensions/Extensions/Enumerable.cs
+++ Extensions/Extensions/Enumerable.cs
@@ -36,6 +36,39 @@ namespace NMyVision.Extensions
             return items;
 
         }
+        
+        /// <summary>
+        /// Asynchronously enumerates the query results and performs the specified action on each element.
+        /// </summary>      
+        /// <typeparam name="T">Type of IEnumerable items</typeparam>
+        /// <param name="items">Source to perform action on</param>
+        /// <param name="action">The System.Action`1 delegate to perform on each element of the System.Collections.Generic.IEnumerable`1.</param>
+        public static Task EachAsync<T>(this IEnumerable<T> items, Func<T, Task> fn)
+            => EachAsync(items, fn, System.Threading.CancellationToken.None);
+
+        /// <summary>
+        /// 
+        /// </summary>        
+        /// <typeparam name="T">Type of IEnumerable items</typeparam>
+        /// <param name="action">The action to perform on each element.</param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <returns></returns>
+        public static async Task EachAsync<T>(this IEnumerable<T> items, Func<T, Task> action, System.Threading.CancellationToken cancellationToken)
+        {
+            IEnumerator<T> enumerator = items.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                bool task;
+                do
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    T current = enumerator.Current;
+                    task = enumerator.MoveNext();
+                    await action(current);
+                }
+                while (task);
+            }
+        }
 
         /// <summary>
         /// 
